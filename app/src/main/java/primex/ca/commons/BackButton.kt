2d8a94e4dc7835646ed20f6717ce1e93751cb3ad
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2022-05-23 03:33:04 -0400

    add commons

---
+++ app/src/main/java/primex/ca/commons/BackButton.kt
@@ -0,0 +1,26 @@
+package primex.ca.commons
+
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import primex.ca.ui.theme.AppOnPrimaryColor
+import primex.ca.ui.theme.ButtonColor
+
+@Composable
+fun BackButton(modifier: Modifier = Modifier, onClick: () -> Unit) {
+    FloatingActionButton(
+        modifier = modifier.size(42.dp),
+        backgroundColor = ButtonColor,
+        contentColor = AppOnPrimaryColor,
+        onClick = { onClick() }) {
+        Icon(
+            imageVector = Icons.Rounded.ArrowBack,
+            contentDescription = "back icon",
+        )
+    }
+}
